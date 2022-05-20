150a2fd1cfa963d93565b09382ea17c5b92bc9ef
Seoeun Lee <72355870+sl980013@users.noreply.github.com>
2022-05-20 04:11:59 +0100

    Updated Post Edit, Delete function

--- travelbuddy/blog/views.py
+++ travelbuddy/blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import PostModel
-from .forms import PostModelForm
+from .forms import PostModelForm, PostUpdateForm
 
 # Create your views here.
 
+# Creating post
 def index(request):
     posts = PostModel.objects.all()
     if request.method == 'POST':
         form = PostModelForm(request.POST)
         if form.is_valid():
             instance = form.save(commit=False)
+            # request.user=logged in user
             instance.author = request.user
             instance.save()
             # blog url in blog urls
@@ -22,3 +24,37 @@ def index(request):
     }
 
     return render(request, 'blog/index.html', context)
+
+# pk for primary key
+def post_detail(request, pk):
+    post = PostModel.objects.get(id=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+def post_edit(request, pk):
+    # grab post
+    post = PostModel.objects.get(id=pk)
+    if request.method == 'POST':
+        form = PostUpdateForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog-post-detail', pk=post.id)
+    else:
+        form = PostUpdateForm(instance=post)
+    context = {
+        'post': post,
+        'form': form,
+    }
+    return render(request, 'blog/post_edit.html',context)
+
+def post_delete(request, pk):
+    post = PostModel.objects.get(id=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog-index')
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_delete.html', context)
